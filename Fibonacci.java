e37f2b7314ce7b83b1ce221af4c3961aec351c1f
madhav2104 <madhav.krishna@ascendion.com>
2026-03-06 10:23:35 +0530

    java syntax correction

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,9 +3,9 @@ public class Fibonacci {
         int n = args.length > 0 ? Integer.parseInt(args[0]) : 10;
         long a = 0, b = 1;
         for (int i = 0; i < n; i++) {
-            System.out.print(a + (i < n - 1 ? " " : ""))
+            System.out.print(a + (i < n - 1 ? " " : ""));
             long t = a + b; a = b; b = t
         }
-        System.out.println()
+        System.out.println();
     }
 }
